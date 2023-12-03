59c707b977699840e31b946f6bbf889ff78e7f33
Sanan Akhverdiev <120374801+sakhverdiev@users.noreply.github.com>
2023-12-03 21:33:39 +0400

    first commit

---
+++ Decorator-Facade-DP/Decorator-DP/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Data;
+
+interface IDataSource
+{
+    void writeData(string data);
+    void readData(string data);
+}
+
+
+class FileDataSource : IDataSource
+{
+    private string fileName;
+
+    public FileDataSource(string filename)
+    {
+        this.fileName = filename;
+    }
+
+    public void writeData(string data)
+    {
+        Console.WriteLine("Data is writing...");
+    }
+
+    public void readData(string data)
+    {
+        Console.WriteLine("Data is reading...");
+    }
+}
+
+
+class DataSourceDecorator : IDataSource
+{
+
+    private IDataSource wrappee;
+
+    public DataSourceDecorator(IDataSource datasource)
+    {
+        wrappee = datasource;
+    }
+
+
+    virtual public void readData(string data)
+    {
+        Console.WriteLine("Data is writing...");
+    }
+
+    virtual public void writeData(string data)
+    {
+        Console.WriteLine("Data is reading...");
+    }
+}
+
+
+
+class EncryptionDecorator : DataSourceDecorator
+{
+    public override void writeData(string data)
+    {
+        base.writeData(data);
+    }
+
+    public override void readData(string data)
+    {
+        base.readData(data);
+    }
+}
+
+
+class CompressionDecorator : DataSourceDecorator
+{
+    public override void writeData(string data)
+    {
+        base.writeData(data);
+    }
+
+    public override void readData(string data)
+    {
+        base.readData(data);
+    }
+}
